dd7d84024f74e64dbb823fc541efea9ab2214bbd
Aaron Tully <aarontully0@gmail.com>
2024-06-05 14:22:18 +1000

    cleaned up recording_info screen

--- lib/screens/recording_info.dart
+++ lib/screens/recording_info.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:just_audio/just_audio.dart';
@@ -8,6 +6,8 @@ import 'package:kubrick/services/ai_api_service.dart';
 import 'package:path/path.dart' as p;
 import 'package:sqflite/sqflite.dart';
 import 'package:sqflite/sqlite_api.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:tuple/tuple.dart';
 
 class RecordingInfoScreen extends StatefulWidget {
   final Recording recording;
@@ -37,29 +37,13 @@ class _RecordingInfoScreenState extends State<RecordingInfoScreen> {
     super.dispose();
   }
 
-  Future<void> deleteRecording() async {
-    File file = File(widget.recording.path!);
-    if (await file.exists()) {
-      await file.delete();
-    }
-
-    await widget.db!.delete(
-      'recordings',
-      where: 'path = ?',
-      whereArgs: [widget.recording.path],
-    );
-    widget.onDelete(widget.recording);
-    // ignore: use_build_context_synchronously
-    Navigator.of(context).pop();
-    setState(() {});
-  }
-
   Future<void> transcribeAudio() async {
     final apiService = ApiService(baseUrl: 'https://transcription.staging.endemolshine.com.au/api/v1');
     final response = await apiService.get('user');
 
     if (response.statusCode == 200) {
-      //final transcription = response.body;
+      final transcription = response.body;
+      print(transcription);
       // Use the 'transcription' variable here
     } else {
       //print('Didnt work');
@@ -69,80 +53,99 @@ class _RecordingInfoScreenState extends State<RecordingInfoScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          PopupMenuButton<String>(
-        onSelected: (String value) {
-          if (value == '1') {
-            transcribeAudio();
-          }
-          else if (value == '2') {
-            deleteRecording();
-          }
-        },
-        itemBuilder: (context) => [
-          const PopupMenuItem(
-            value: '1',
-            child: Text('Transcribe'),
-          ),
-          const PopupMenuItem(
-            value: '2',
-            child: Text('Delete'),
-          )
-        ],
-        icon: const Icon(Icons.menu),
-          ),
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(p.basename(widget.recording.path!)),
-            Text(DateFormat('yyyy-MM-dd HH:mm').format(widget.recording.createdAt)),
-            StreamBuilder(
-              stream: positionStream,
-              builder: (context, snapshot) {
-                final position = snapshot.data ?? Duration.zero;
-                final duration = player.duration ?? Duration.zero;
-                return Slider(
-                  onChanged: (value) {
-                    player.seek(Duration(seconds: value.toInt()));
-                  },
-                  value: position.inSeconds.toDouble(),
-                  min: 0.0,
-                  max: duration.inSeconds.toDouble(),
-                );
+      appBar: AppBar(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+            child: ElevatedButton(
+              style: ButtonStyle(
+                padding: WidgetStateProperty.all<EdgeInsets>(
+                  const EdgeInsets.all(20.0)
+                )
+              ),
+              onPressed: () {
+                transcribeAudio();
               },
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                StreamBuilder(
-                  stream: player.playingStream,
-                  builder: (context, snapshot) {
-                    final isPlaying = snapshot.data ?? false;
-                    return IconButton(
-                      onPressed: () {
-                        if (isPlaying) {
-                          player.pause();
-                        } else {
-                          player.play();
-                        }
-                      },
-                      icon: Icon(
-                        isPlaying ? Icons.pause : Icons.play_arrow,
-                        size: 48,
-                      ),
-                      color: Colors.blue,
-                      splashRadius: 24,
-                    );
-                  },
+              child: const Text(
+                'Transcribe',
+                style: TextStyle(
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
                 ),
-              ],
-            )
-          ],
-        ),
+              ),
+            ),
+          ),
+            Expanded(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Text(p.basename(widget.recording.path!)),
+                  Text(DateFormat('yyyy-MM-dd HH:mm').format(widget.recording.createdAt)),
+                  StreamBuilder(
+                    stream: positionStream,
+                    builder: (context, snapshot) {
+                      final position = snapshot.data ?? Duration.zero;
+                      final duration = player.duration ?? Duration.zero;
+                      return Slider(
+                      onChanged: (value) {
+                        player.seek(Duration(seconds: value.toInt()));
+                      },
+                      value: position.inSeconds.toDouble(),
+                      min: 0.0,
+                      max: duration.inSeconds.toDouble(),
+                      );
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 30.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        StreamBuilder(
+                          stream: Rx.combineLatest2<bool, ProcessingState, Tuple2<bool, ProcessingState>>(
+                            player.playingStream,
+                            player.processingStateStream,
+                            (playing, processingState) => Tuple2(playing, processingState),
+                          ),
+                          builder: (context, snapshot) {
+                            final data = snapshot.data;
+                            final isPlaying = data?.item1 ?? false;
+                            final processingState = data?.item2 ?? ProcessingState.idle;
+                            final isCompleted = processingState == ProcessingState.completed;
+                            return IconButton(
+                              onPressed: () {
+                                if(!isCompleted){
+                                  if (isPlaying) {
+                                    player.pause();
+                                  } else {
+                                    player.play();
+                                  }
+                                } else {
+                                  player.seek(Duration.zero);
+                                  player.play();
+                                }
+                              },
+                              icon: Icon(
+                                isPlaying && !isCompleted ? Icons.pause : Icons.play_arrow,
+                                size: 48,
+                              ),
+                              color: Colors.blue,
+                              splashRadius: 24,
+                              );
+                            },
+                          ),
+                        ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
